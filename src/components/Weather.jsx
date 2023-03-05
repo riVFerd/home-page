8286db6da3655687cc53e1b50541b6fb5d2b4264
Virgy Ferdian Surya Firmansyah <virgyferdian213@gmail.com>
2023-03-06 05:28:40 +0700

    add weather component

---
+++ src/components/Weather.jsx
@@ -0,0 +1,34 @@
+import {useEffect, useState} from "react";
+import {CiTempHigh, CiDroplet} from "react-icons/ci";
+import {BsDropletHalf} from "react-icons/bs";
+import {FaTemperatureLow} from "react-icons/fa";
+
+const Weather = (props) => {
+    const [weatherData, setWeatherData] = useState();
+    const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+    const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=malang&aqi=no`;
+
+    useEffect(() => {
+        fetch(url).then(response => response.json()).then(data => {
+            setWeatherData(data.current);
+        });
+    }, []);
+
+    return (
+        <div id={props.id} className={'flex flex-col items-center gap-2'}>
+            {
+                (!weatherData)
+                    ? <p>Loading...</p>
+                    : <>
+                    <img className={'w-[64px]'} src={weatherData['condition'].icon}/>
+                    <div className={'flex gap-8'}>
+                        <div className={'flex items-center gap-1'}><FaTemperatureLow/><span>{weatherData['temp_c']}°C</span></div>
+                        <div className={'flex items-center gap-1'}><BsDropletHalf/><span>{weatherData['humidity']}%</span></div>
+                    </div>
+                    </>
+            }
+        </div>
+    )
+}
+
+export default Weather;
