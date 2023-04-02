226fc5758140d7afb6b578f6ec7673f89d626f12
Virgy Ferdian Surya Firmansyah <virgyferdian213@gmail.com>
2023-04-02 20:30:48 +0700

    add greeting text

--- src/components/DateAndClock.jsx
+++ src/components/DateAndClock.jsx
@@ -19,22 +19,47 @@ const getTime = (date) => {
     }
 }
 
+const getUsername = () => {
+    const username = localStorage.getItem('username');
+    if (username) return username;
+    localStorage.setItem('username', 'Virgy');
+    return 'Virgy';
+}
+
 const DateAndClock = (props) => {
     const dateTime = new Date();
     const [date, setDate] = useState(getDate(dateTime));
     const [time, setTime] = useState(getTime(dateTime));
+    const [greeting, setGreeting] = useState('Hello');
 
     useEffect(() => {
         setInterval(() => {
             const newDateTime = new Date()
             setDate(getDate(newDateTime))
             setTime(getTime(newDateTime))
+
+            // change greeting based on time of day
+            const currentHours = newDateTime.getHours();
+            if (currentHours === time.hours) return; // don't update greeting if time hasn't changed
+            switch (true) {
+                case (currentHours >= 5 && currentHours < 10):
+                    setGreeting("Good Morning");
+                    break;
+                case (currentHours >= 10 && currentHours < 15):
+                    setGreeting("Good Afternoon");
+                    break;
+                case (currentHours >= 15 && currentHours < 19):
+                    setGreeting("Good Evening");
+                    break;
+                default:
+                    setGreeting("Good Night");
+            }
         }, 1000);
     }, []);
 
     return (
         <div id={props.id} className={'container flex flex-col' + ` ${props.className}`}>
-            <div id={'date'} className={''}>{date}</div>
+            <h1 className="text-2xl font-bold py-1">{greeting}, {getUsername()}</h1>
             <div id={'clock'}>
                 <p className={'text-4xl font-bold'}>
                     <span>{time.hours}</span>
@@ -44,6 +69,7 @@ const DateAndClock = (props) => {
                     <span>{time.seconds}</span>
                 </p>
             </div>
+            <div id={'date'} className={''}>{date}</div>
         </div>
     );
 }
